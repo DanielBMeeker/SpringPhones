502b529fa239a6926ebfe2a913ac94afd0c597b9
DanielBMeeker <54420579+DanielBMeeker@users.noreply.github.com>
2019-10-24 21:10:39 -0500

    Added a few comments

--- src/main/java/dmacc/SpringPhonesApplication.java
+++ src/main/java/dmacc/SpringPhonesApplication.java
@@ -9,38 +9,39 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-
 import dmacc.beans.Phone;
 import dmacc.controller.BeanConfiguration;
 import dmacc.repository.PhoneRepository;
 
 @SpringBootApplication
-public class SpringPhonesApplication implements CommandLineRunner{
+public class SpringPhonesApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringPhonesApplication.class, args);
-ApplicationContext appContext = new AnnotationConfigApplicationContext(BeanConfiguration.class);
-		
-		Phone p = appContext.getBean("phone", Phone.class);
-		
-		System.out.println(p.toString());
+//ApplicationContext appContext = new AnnotationConfigApplicationContext(BeanConfiguration.class);
+//		
+//		Phone p = appContext.getBean("phone", Phone.class);
+//		
+//		System.out.println(p.toString());
 	}
+
 	@Autowired
 	PhoneRepository repo;
-	
+
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		ApplicationContext appContext = new AnnotationConfigApplicationContext(BeanConfiguration.class);
+		//This Phone P will add the phone we made in the bean configuration to the database
 		Phone p = appContext.getBean("phone", Phone.class);
-		
 		repo.save(p);
-		//Creating an object to use – not managed by Spring
-				Phone d = new Phone("515-123-4567", "Samsung", "Galaxy S10", 64);
-				repo.save(d);
-				List<Phone> allPhones = repo.findAll();
-				for(Phone phone: allPhones) {
-					System.out.println(phone.toString());
-				}
-
+		//This Phone D will not use the bean but instead the phone we create with this constructor
+		//to persist to the database
+		Phone d = new Phone("515-123-4567", "Samsung", "Galaxy S10", 64);
+		repo.save(d);
+		List<Phone> allPhones = repo.findAll();
+		for (Phone phone : allPhones) {
+			System.out.println(phone.toString());
+		}
+		((AnnotationConfigApplicationContext) appContext).close();
 	}
 }
